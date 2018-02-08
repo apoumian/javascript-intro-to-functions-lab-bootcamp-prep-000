8310e19470ba1ddc0421c567a189448f39abdfd0
Arturo Poumian <apoumians@gmail.com>
2018-02-08 16:20:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function logWhisper(string) {
 
 function sayHiToMama(string) {
   var lowercase = string
-  if
-  lowercase.toLowerCase() === lowercase
-  return "I can\'t hear you!"
+  if (lowercase.toLowerCase() === lowercase)
+  return `I can\'t hear you!`
 }
